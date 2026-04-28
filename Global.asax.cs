68c818e8c9c41803bad1ae235466b5a0d73f837f
agostomat8-svg <agostomat8@gmail.com>
2026-04-28 18:32:05 +0800

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProcessController
+{
+	public class Global : System.Web.HttpApplication
+	{
+		void Application_Start(object sender, EventArgs e)
+		{
+			// ❌ REMOVE Optimization and Bundles (causes crash)
+
+			// If you are NOT using routing, leave this EMPTY
+			// This is safe for WebForms projects
+		}
+	}
+}
